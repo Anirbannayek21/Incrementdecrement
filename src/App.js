e48c5ec6fa189e2ad4253b7ef1aeee3f53d0a3b1
Anirbannayek21 <anirbannayek2@gmail.com>
2021-04-01 13:17:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Button from './Button';
 
 function App() {
+
+  const [number, setnumber] = useState(0)
+
+  const increament = () => {
+    setnumber(number + 1);
+  }
+  const decreament = () => {
+    setnumber((value) => {
+      if (value < 1) {
+        setnumber(0)
+        return alert("reach 0");
+      }
+      else {
+        return value - 1;
+      }
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="back">
+        <div className="box">
+          <div className="box1">
+            <h1>{number}</h1>
+            <div>
+              <Button text="INC" bcolor="#3333ff" onSelect={increament} />
+              <Button text="DEC" bcolor="#ff3300"
+                onSelect={decreament} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
